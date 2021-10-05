19c52f75770c0d6cc5c09b461d52c92d7761c881
E.I.A <llhejunwei@126.com>
2021-10-05 11:07:28 +0800

    first commit3

--- src/main/java/TEST.java
+++ src/main/java/TEST.java
@@ -2,5 +2,6 @@ public class TEST {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("first commit2");
+        System.out.println("hot-fix commit2");
     }
 }
